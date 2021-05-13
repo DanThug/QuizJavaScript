88a0bb661f67b8c6c5475994d336f45b1d0ae8ea
Daniel Ferreira <danthuggin@gmail.com>
2021-05-13 20:18:52 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+/*
+Este exercício será um pouquinho diferente dos anteriores.
+
+Seu desafio é desenvolver uma versão do quiz que:
+
+- Aborda um tema diferente (não pode ser de filmes);
+- Tem um tema de cores diferente do que foi apresentado na aula;
+- Exibe na tela a pontuação que o usuário fez. Não há certo ou errado, apenas faça. Essa exibição de pontos é uma das implementações que faremos na próxima aula =D
+
+Independente se você já fez o quiz dos filmes enquanto acompanhava a aula, tente fazer esse exercício sem rever partes da aula.
+
+É importante que a sua versão do quiz seja feita apenas com o conteúdo que vimos até aqui.
+
+Depois de fazer o que foi pedido acima, crie um repositório no GitHub para a sua aplicação e abra uma issue no repositório do curso com:
+
+- O link da sua versão do quiz;
+- Quais foram as suas maiores dúvidas ou dificuldades durante a execução desse exercício;
+- Quais foram as suas menores dificuldades durante a execução desse exercício.
+
+Link do repositório do curso: https://github.com/roger-melo-treinamentos/curso-de-js-roger-melo/issues
+
+Ps: se você não conseguiu fazer tudo o que foi pedido acima, abra a issue mesmo assim =)
+*/
+
+const form = document.querySelector('form');
+const quizResultMessage = document.querySelector('.quizResultMessage');
+const resetButton = form.resetButton;
+
+resetButton.addEventListener('click', () => {
+    quizResultMessage.setAttribute('class', 'quizResultMessage');
+    quizResultMessage.textContent = '';
+});
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const quiz = {
+        correctAnswers: ['B','A','A','B'],
+        userAnswers: [
+            e.target.userAnswer1.value,
+            e.target.userAnswer2.value,
+            e.target.userAnswer3.value,
+            e.target.userAnswer4.value
+        ],
+        getScore() {
+            let maxScore = 0;
+
+            this.userAnswers.forEach((answer, i) => {
+                const match = answer === this.correctAnswers[i];
+                if (match) {
+                    maxScore += 25;
+                }
+            });
+            return maxScore;
+        },
+        showResult() {
+            const score = this.getScore();
+
+            switch (score) {
+                case 100:
+                    quizResultMessage.setAttribute('class', 'quizResultMessage alert alert-success');
+                    break;
+                case 75:
+                    quizResultMessage.setAttribute('class', 'quizResultMessage alert alert-primary');
+                    break;
+                case 50:
+                    quizResultMessage.setAttribute('class', 'quizResultMessage alert alert-warning');
+                    break;      
+                default:
+                    quizResultMessage.setAttribute('class', 'quizResultMessage alert alert-danger');
+                    break;
+            }
+            return quizResultMessage.textContent = `Sua taxa de acerto foi de ${score}%`;
+        },
+    };    
+
+    quiz.showResult();
+});
